5fcd7674369da9040440b4a95544b9566ca78b11
Matthew <matthewfalcone5@gmail.com>
2026-04-01 22:01:34 -0400

    pages structurer

--- A3/frontend/src/main.tsx
+++ A3/frontend/src/main.tsx
@@ -1,14 +1,16 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
+import { BrowserRouter, Route, Routes } from "react-router"
 
 import "./index.css"
 import App from "./App.tsx"
-import { ThemeProvider } from "@/components/theme-provider.tsx"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 )

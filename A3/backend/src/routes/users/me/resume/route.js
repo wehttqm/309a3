8c33e37a0d126c52788c36a15479d06a5e41f816
Matthew <matthewfalcone5@gmail.com>
2026-04-05 12:19:00 -0400

    should be resume not avatar

--- A3/backend/src/routes/users/me/resume/route.js
+++ A3/backend/src/routes/users/me/resume/route.js
@@ -4,14 +4,14 @@ const PUT = async (req, res) => {
   try {
     if (!req.file) return res.status(400).json({ error: "File is required." });
 
-    const avatarPath = `/uploads/users/${req.auth.id}/resume/${req.file.filename}`;
+    const resumePath = `/uploads/users/${req.auth.id}/resume/${req.file.filename}`;
 
     await prisma.user.update({
       where: { id: req.auth.id },
-      data: { avatar: avatarPath },
+      data: { resume: resumePath },
     });
 
-    return res.status(200).json({ avatar: avatarPath });
+    return res.status(200).json({ resume: resumePath });
   } catch (error) {
     return res.status(500).json({ error: "Internal server error." });
   }
